50f9d83167e488451bb01875f8abc333c3cca73e
therealmive477-commits <therealmive477@gmail.com>
2026-04-13 12:17:45 -0500

    Add API endpoint to create events

---
+++ pages/api/events/create.js
@@ -0,0 +1,14 @@
+import { supabase } from "../../../lib/supabase";
+import { v4 as uuid } from "uuid";
+
+export default async function handler(req, res) {
+
+  const id = uuid().slice(0,6);
+
+  await supabase
+    .from("events")
+    .insert([{ id }]);
+
+  res.status(200).json({ id });
+
+}
