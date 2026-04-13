b75b09baabd4585ef336469ba45bd6b6127b1ee2
therealmive477-commits <therealmive477@gmail.com>
2026-04-13 12:23:00 -0500

    Add API endpoint to handle song voting

---
+++ pages/api/events/vote.js
@@ -0,0 +1,20 @@
+import { supabase } from "../../../lib/supabase";
+
+export default async function handler(req,res){
+
+  const { songId } = req.body;
+
+  const { data } = await supabase
+    .from("songs")
+    .select("votes")
+    .eq("id", songId)
+    .single();
+
+  await supabase
+    .from("songs")
+    .update({ votes: data.votes + 1 })
+    .eq("id", songId);
+
+  res.status(200).json({ success:true });
+
+}
