97b38c96b7293603e1a11848a0b2cf19f24e30ba
therealmive477-commits <therealmive477@gmail.com>
2026-04-13 12:21:33 -0500

    Add API endpoint to request song insertion

---
+++ pages/api/events/request.js
@@ -0,0 +1,19 @@
+import { supabase } from "../../../lib/supabase";
+
+export default async function handler(req,res){
+
+  const { eventId, title, artist } = req.body;
+
+  await supabase
+    .from("songs")
+    .insert([
+      {
+        event_id: eventId,
+        title,
+        artist
+      }
+    ]);
+
+  res.status(200).json({ success:true });
+
+}
